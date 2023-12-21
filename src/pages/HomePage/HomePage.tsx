be912f62e9b049a4854c273de767cb8be8d6365f
Shevtsov Vladislav <71644947+EyeSeeIce@users.noreply.github.com>
2023-12-21 19:48:52 +0300

    change ci file

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,32 @@
 import {HomeScreen} from "../../screens/HomeScreen";
+import {useSearchParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const HomePage = () => {
+    const [params] = useSearchParams();
+
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/todos/${randomIntFromInterval(1, 5)}`).then(response => {
+            setTimeout(() => {
+                setData(response.data.title)
+            }, 500)
+        })
+    }, [])
     return (
-        <HomeScreen />
+        <>
+            <h1>TITLE: {data}</h1>
+            <h1>{params.get('search')}</h1>
+            <HomeScreen />
+        </>
     );
 };
 
 export default HomePage;
+
+
+const randomIntFromInterval = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
