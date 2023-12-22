0e0e97568b262d1c48c7efc5520ef63b01eab9dc
Shevtsov Vladislav <71644947+EyeSeeIce@users.noreply.github.com>
2023-12-22 09:02:19 +0300

    Revert "test"
    This reverts commit db8a40f66e6e2b52173c5d5f1ff01ceba028bbe0.

--- src/utils/router/router.tsx
+++ src/utils/router/router.tsx
@@ -2,7 +2,10 @@ import {createBrowserRouter} from "react-router-dom";
 import {HomePage} from "../../pages/HomePage";
 
 const router = createBrowserRouter([
-    {path: "/", element: <HomePage/>,},
+    {
+        path: "/",
+        element: <HomePage/>,
+    },
 ]);
 
 export default router
