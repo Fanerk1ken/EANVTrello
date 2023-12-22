db8a40f66e6e2b52173c5d5f1ff01ceba028bbe0
Shevtsov Vladislav <71644947+EyeSeeIce@users.noreply.github.com>
2023-12-22 08:50:40 +0300

    test

--- src/utils/router/router.tsx
+++ src/utils/router/router.tsx
@@ -2,10 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import {HomePage} from "../../pages/HomePage";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage/>,
-    },
+    {path: "/", element: <HomePage/>,},
 ]);
 
 export default router
