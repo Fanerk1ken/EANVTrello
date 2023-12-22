595cae7374d51403382025597db9216efb1f63ee
Egor Nikandrov <egr.nikandrov@gmail.com>
2023-12-22 18:39:59 +0300

    add props destructurization

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,6 +2,7 @@ import style from "./Card.module.scss";
 import {AiOutlineAlignLeft} from "react-icons/ai";
 import {FC} from "react";
 
+
 type CardProps = {
     id: number
     title: string
@@ -9,10 +10,11 @@ type CardProps = {
 }
 
 const Card:FC<CardProps>= (props) => {
+    const {id, title} = props
     return (
         <div className={style.card}>
-            <span className={style.cardTitle}>{props.title}</span>
-            <span className={style.taskId}><AiOutlineAlignLeft />FTL-{props.id}</span>
+            <span className={style.cardTitle}>{title}</span>
+            <span className={style.taskId}><AiOutlineAlignLeft />FTL-{id}</span>
         </div>
     );
 };
