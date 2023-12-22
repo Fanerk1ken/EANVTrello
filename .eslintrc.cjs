ed979013d3b6b61874eea3fd9479425dde9bddeb
Shevtsov Vladislav <71644947+EyeSeeIce@users.noreply.github.com>
2023-12-22 08:49:22 +0300

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,16 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-}
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
+
