e97159d3e0dd2ac615280ed4c36e4725c9a8ed87
Shevtsov Vladislav <71644947+EyeSeeIce@users.noreply.github.com>
2023-12-22 09:02:20 +0300

    Revert "test"
    This reverts commit ed979013d3b6b61874eea3fd9479425dde9bddeb.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,18 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    'prettier/prettier': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-};
-
+}
