cb8ec1b5fb81738db85b6751350e4e75f449b358
Egor Nikandrov <egr.nikandrov@gmail.com>
2023-12-22 22:58:08 +0300

    fix Button component, add new variables, create ColumnGroup

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -1,25 +1,21 @@
 import {ButtonHTMLAttributes} from "react";
-import {FC} from "react";
-import style from "../../screens/HomeScreen/HomeScreen.module.scss";
-import {BsPlusLg} from "react-icons/bs";
 import {MouseEvent} from "react";
+import {forwardRef} from "react";
+import style from './Button.module.scss'
 
+type ButtonProps = {
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
-type ButtonProps = {} & ButtonHTMLAttributes<HTMLButtonElement>
-
-const Button: FC<ButtonProps> = (props) => {
-
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(( props, ref) => {
     const onClick = (event: MouseEvent<HTMLButtonElement>) => {
         if (props.onClick) {
             props.onClick(event)
         }
     }
 
-
     return (
-        <button type={props.type} onClick={onClick} className={style.addColumnButton}><BsPlusLg
-            className={style.plusSVG}/>{props.children}</button>
+        <button ref={ref} className={props.className ? props.className : style.addCardButton} type={props.type} onClick={onClick}>{props.children}</button>
     );
-};
+})
 
 export default Button;
---
+++ src/components/ColumnGroup/ColumnGroup.tsx
@@ -0,0 +1,71 @@
+import {useState} from "react";
+import {useRef} from "react";
+import {useClickOutside} from "../../utils/hook/useClickOutside.ts";
+import {useForm} from "react-hook-form";
+import {Column} from "../Column";
+import Button from "../Buttons/Button.tsx";
+import {BsPlusLg} from "react-icons/bs";
+import style from './ColumnGroup.module.scss'
+import { IoCloseOutline } from "react-icons/io5";
+
+type Inputs = {
+    value: string
+    valueRequired: string
+}
+
+type ColumnType = {
+    title: string
+    tasks: any[]
+}
+const ColumnGroup = () => {
+    const [columns, setColumns] = useState<ColumnType[]>([])
+    const [openNewColomn, setOpenNewColomn] = useState(false)
+    const clickRef = useRef(null);
+    const clickButtonRef = useRef(null);
+    useClickOutside([clickRef, clickButtonRef], () => {
+        setOpenNewColomn(false)
+    });
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+    } = useForm<Inputs>()
+
+    const onClick = () => {
+        setOpenNewColomn(true)
+    }
+
+    const handleClickClose = () => {
+        setOpenNewColomn(false)
+    }
+
+    const onSubmit = handleSubmit((data) => {
+        // я пидорас и исправлю это (нормальные типы для тасок)
+        setColumns((prevState) => [...prevState, {title: data.value, tasks: []}])
+        reset()
+        setOpenNewColomn(false)
+    })
+    return (
+        <div>
+            {columns.map(el => <Column key={Date.now()} title={el.title} tasks={[]}/>)}
+            {openNewColomn ? <div ref={clickRef}>
+                <form onSubmit={onSubmit} className={style.form}>
+                    <textarea autoFocus={true}
+                              className={style.inputAddColumn}
+                              placeholder="Ввести заголовок для списка" cols={30}
+                              rows={10} {...register("value", {required: true})}></textarea>
+                    <div className={style.openAddCardWithCloseIcons}>
+                        <Button className={style.addNewColumnButton} type={"submit"} onSubmit={onSubmit}>
+                            Добавить колонку
+                        </Button>
+                        <IoCloseOutline onClick={handleClickClose} className={style.closeSvg} color={'white'} />
+                    </div>
+
+                </form>
+            </div> : <Button onClick={onClick} className={style.addColumnButton}><BsPlusLg/>Добавить еще одну колонку</Button>}
+        </div>
+    );
+};
+
+export default ColumnGroup;
